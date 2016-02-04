9c562cd5e5ad6e0e12c7d315db4230d5f1321c37
Matthias Stevens <matthias.stevens@gmail.com>
2016-02-04 11:42:46 +0100

    Wrapped import and call to register in try/except
    this makes it safe to run __init__.py before geokey is installed (e.g. to grab the __version__ during extension setup)
    
    Signed-off-by: Matthias Stevens <matthias.stevens@gmail.com>

--- geokey_extension/__init__.py
+++ geokey_extension/__init__.py
@@ -1,13 +1,15 @@
-from geokey.extensions.base import register
-
-
 VERSION = (0, 0, 0)
 __version__ = '.'.join(map(str, VERSION))
 
-register(
-    'geokey_extension',
-    'My Awesome Extension',
-    display_admin=True,
-    superuser=False,
-    version=__version__
-)
+
+try:
+    from geokey.extensions.base import register
+    register(
+        'geokey_extension',
+        'My Awesome Extension',
+        display_admin=True,
+        superuser=False,
+        version=__version__
+    )
+except BaseException:
+    print 'Please install GeoKey first'
